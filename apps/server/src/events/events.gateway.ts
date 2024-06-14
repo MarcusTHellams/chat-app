44455d9a4b760b945838c4fcea44f7671eba2a7a
Marcus Hellams <mhellams@hotmail.com>
2024-06-14 09:31:37 -0400

    socket work on backend

--- apps/server/src/events/events.gateway.ts
+++ apps/server/src/events/events.gateway.ts
@@ -1,5 +1,7 @@
 import {
+  ConnectedSocket,
   MessageBody,
+  OnGatewayConnection,
   SubscribeMessage,
   WebSocketGateway,
   WebSocketServer,
@@ -8,19 +10,70 @@ import {
 import { from, Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
-import { Server } from 'socket.io';
+import { Server, Socket } from 'socket.io';
+import { UsersService } from '../users/users.service';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+const onlineUser = new Set();
 
 @WebSocketGateway({
   cors: {
     origin: '*',
+    credentials: true,
   },
 })
-export class EventsGateway {
+export class EventsGateway implements OnGatewayConnection {
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly prisma: PrismaService,
+  ) {}
   @WebSocketServer()
   server: Server;
 
+  async handleConnection(socket: Socket) {
+    console.log('connect User ', socket.id);
+    const token: string = socket.handshake.auth.token;
+    const user = await this.usersService.getUserDetailsFromToken(token);
+    if ('userId' in user) {
+      socket.join(`${user.userId}`);
+      onlineUser.add(`${user.userId}`);
+      socket.emit('onlineUser', Array.from(onlineUser));
+    }
+  }
+
+  @SubscribeMessage('message-page')
+  async onMessagePage(
+    @MessageBody() userId: number,
+    @ConnectedSocket() socket: Socket,
+  ) {
+    console.log('userId: ', userId);
+    const userDetails = await this.prisma.user.findUnique({
+      where: { userId },
+    });
+    const payload = {
+      userId: userDetails?.userId,
+      name: userDetails?.name,
+      email: userDetails?.email,
+      profilePic: userDetails?.profilePic,
+      online: onlineUser.has(`${userId}`),
+    };
+    socket.emit('message-user', payload);
+    const conversationMessage = await this.prisma.conversation.findFirst({
+      where: {
+        OR: [
+          {
+
+          }
+        ],
+      },
+    });
+  }
+
   @SubscribeMessage('events')
-  findAll(@MessageBody() data: any): Observable<WsResponse<number>> {
+  findAll(
+    client: Socket,
+    @MessageBody() data: any,
+  ): Observable<WsResponse<number>> {
     return from([1, 2, 3]).pipe(
       map((item) => ({ event: 'events', data: item })),
     );
--- apps/server/src/events/events.module.ts
+++ apps/server/src/events/events.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { EventsGateway } from './events.gateway';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
-  providers: [EventsGateway]
+  imports: [UsersModule],
+  providers: [EventsGateway],
 })
 export class EventsModule {}
